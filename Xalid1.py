abcf7635710ca5226b599dc8e295723b95668f08
Hacıyeva Aytən <88345991+ayten21@users.noreply.github.com>
2022-08-18 14:13:45 +0400

    Update Xalid1.py

--- Xalid1.py
+++ Xalid1.py
@@ -7,9 +7,8 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_recall_f
 import tensorflow as tf
 import zipfile
 class HelperFunc:
-    def __init__(self):
-        self.data = input('enter data name: ')
-        data = self.data
+    global data
+    data = input('enter name')
     def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
         cm = confusion_matrix(y_true, y_pred)
         cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] 
